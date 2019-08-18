f6ae148087b22271e4ca378d5c596b3a3662afc6
codeblue001 <39382665+codeblue001@users.noreply.github.com>
2019-08-18 17:56:58 -0400

    I will create layout and add resource files

--- app/src/main/java/jide/delano/starrysky/view/MainActivity.java
+++ app/src/main/java/jide/delano/starrysky/view/MainActivity.java
@@ -1,14 +1,16 @@
-package jide.delano.starrysky;
+package jide.delano.starrysky.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import jide.delano.starrysky.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_input);
     }
 }
